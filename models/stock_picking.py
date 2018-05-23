b686a5ead6aceaca830bc8158e64e16352c7881f
Jose M. Coronado <j.coronado.personal@gmail.com>
2018-05-22 21:27:25 -0700

    [IMP] Changed the shipstation integration to clients request

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -8,7 +8,6 @@ class Picking(models.Model):
     _inherit = "stock.picking"
 
     ss_id = fields.Integer(string="Shipstation ID", copy=False)
-    ss_ship_id = fields.Integer(string="Shipment ID", copy=False)
     ss_status = fields.Selection([
         ('awaiting_payment', 'Awaiting Payment'),
         ('awaiting_shipment', 'Awaiting Shipment'),
@@ -29,23 +28,28 @@ class Picking(models.Model):
         ('adult_signature', 'Adult Signature'),
         ('direct_signature', 'Direct Signature'),
         ], string='Confirmation method', default='none',copy=False)
+    do_not_send = fields.Boolean(string="Do Not Send to Shipstaion")
+    customer_ship_account = fields.Many2one('x_customer.shipping.account',string="Third Party Account")
+
+    @api.constrains('partner_id')
+    def set_shipping_account(self):
+        for record in self:
+            accounts = record.env['x_customer.shipping.account'].search([('x_partner_id','=',record.partner_id.id)])
+            if accounts:
+                if record.carrier_id:
+                    refined_accounts = accounts.search([('x_carrier_id','=',record.carrier_id.id)])
+                    if refined_accounts:
+                        record.customer_ship_account = refined_accounts[0]
+                else:
+                    record.customer_ship_account = accounts[0]
 
     @api.multi
     def do_transfer(self):
         super(Picking, self).do_transfer()
         for record in self:
-            if record.ss_id != 0 and record.ss_status not in ['cancelled','shipped']:
-                record.ship_ss()
+            if record.picking_type_code == 'outgoing' and record.do_not_send != True:
+                record.create_update_ssorder()
             return True
-
-    @api.multi
-    def action_cancel(self):
-        super(Picking, self).action_cancel()
-        for record in self:
-            if record.ss_id != 0 and record.ss_status not in ['cancelled','shipped']:
-                record.delete_ssorder()
-            return True
-
 
     @api.multi
     def create_update_ssorder(self):
@@ -89,6 +93,17 @@ class Picking(models.Model):
                     "items": order_item_list,
                     "orderStatus": record.ss_status or "awaiting_shipment",
                 }
+            if record.customer_ship_account:
+                add_option = {
+                    "advancedOptions": {
+                      "billToParty":"third_party",
+                      "billToAccount": record.customer_ship_account.x_acct_num,
+                      "billToPostalCode": record.customer_ship_account.x_zip,
+                      "billToCountryCode": record.customer_ship_account.x_partner_id.country_id.code or "US",
+                      "customField1": record.customer_ship_account.x_carrier_id.name
+                    },
+                }
+                python_dict.update(add_option)
             data_to_post = json.dumps(python_dict)
             conn = company.shipstation_connection(url,'POST',data_to_post)
             response = conn[0]
@@ -106,91 +121,3 @@ class Picking(models.Model):
                 'ss_status': json_object['orderStatus'],
                 'ss_address_status': json_object['shipTo']['addressVerified'],
             })
-
-    @api.multi
-    def create_ss_label(self):
-        for record in self:
-            company = record.env.user.company_id
-            url = '%s/orders/createlabelfororder' % (company.shipstation_root_endpoint)
-            if not record.carrier_id or not record.carrier_id.service_code:
-                raise UserError(_("Either the picking does not have a carrier selected or the carrier is not configured correctly."))
-            python_dict =   {
-                "orderId": record.ss_id,
-                "carrierCode": record.carrier_id.service_code.carrier_code,
-                "serviceCode": record.carrier_id.service_code.service_code,
-                "packageCode": "package",
-                "confirmation": record.confirmation,
-                "shipDate": datetime.datetime.now().strftime('%Y-%m-%d'),
-                "testLabel": False,
-                "weight": {
-                  "value": record.weight,
-                  "units": "pounds"
-                },
-              }
-            data_to_post = json.dumps(python_dict)
-            conn = company.shipstation_connection(url,'POST',data_to_post)
-            response = conn[0]
-            content = conn[1]
-            json_object_str = content
-            json_object = json.loads(json_object_str)
-            if response.status_code != requests.codes.ok:
-                raise UserError(_("%s\n%s: %s" % (url,response.status_code,json_object['ExceptionMessage'])))
-            record.write({
-                'ss_ship_id': json_object['shipmentId'],
-                'carrier_tracking_ref': json_object['trackingNumber'],
-                'ss_status': 'awaiting_shipment',
-            })
-            logmessage = ("""<b>Shipstation</b> %s label for order <b>%s</b> has been <b>created</b> via the API.\n
-            Tracking: %s\n
-            Shipment Cost: %s\n
-            Insurance Cost: %s""") % (record.carrier_id.name,record.ss_id,
-            record.carrier_tracking_ref,
-            json_object['shipmentCost'],
-            json_object['insuranceCost'])
-            record.message_post(body=logmessage, attachments=[('Label_%s-%s.%s' % (record.carrier_id.service_code.carrier_code,record.carrier_tracking_ref,'pdf'),
-             binascii.a2b_base64(json_object['labelData']))])
-
-    @api.multi
-    def ship_ss(self):
-        for record in self:
-            company = record.env.user.company_id
-            url = '%s/orders/markasshipped' % (company.shipstation_root_endpoint)
-            python_dict = {
-                "orderId": record.ss_id,
-                "carrierCode": record.carrier_id.service_code.carrier_code,
-                "shipDate": datetime.datetime.strptime(record.date_done,'%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d'),
-                "trackingNumber": record.carrier_tracking_ref or None,
-                "notifyCustomer": False,
-                "notifySalesChannel": False
-            }
-            data_to_post = json.dumps(python_dict)
-            conn = company.shipstation_connection(url,'POST',data_to_post)
-            response = conn[0]
-            content = conn[1]
-            json_object_str = content
-            json_object = json.loads(json_object_str)
-            if response.status_code != requests.codes.ok:
-                raise UserError(_("%s\n%s: %s" % (url,response.status_code,json_object['ExceptionMessage'])))
-            record.write({
-                'ss_status': 'shipped',
-            })
-            record.message_post(body=_("<b>Shipstation</b> order <b>%s</b> has been marked as <b>shipped</b> via the API.") % (json_object['orderId']))
-
-    @api.multi
-    def delete_ssorder(self):
-        for record in self:
-            company = record.env.user.company_id
-            if not record.ss_id:
-                raise UserError(_("There is no shipstation order linked to this transfer."))
-            url = '%s/orders/%s' % (company.shipstation_root_endpoint,record.ss_id)
-            conn = company.shipstation_connection(url,'DELETE',False)
-            response = conn[0]
-            content = conn[1]
-            if response.status_code != requests.codes.ok:
-                raise UserError(_("%s\n%s: %s" % (url,response.status_code,content)))
-            json_object_str = content
-            json_object = json.loads(json_object_str)
-            record.write({
-                'ss_status': 'cancelled',
-            })
-            record.message_post(body=_("<b>Shipstation</b> order <b>%s</b> %s") % (record.ss_id,json_object['message']))
